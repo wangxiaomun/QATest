b79f69248926c2cc6ad2b3fbff6b7106d8dafc36
xiao <xiao@heyorca.com>
2018-11-06 12:25:06 -0330

    init project

---
+++ src/qa/test/testcases/BaseTest.java
@@ -0,0 +1,28 @@
+package qa.test.testcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+
+public class BaseTest {
+  @Test(dataProvider = "dp")
+  public void f(Integer n, String s) {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+}
